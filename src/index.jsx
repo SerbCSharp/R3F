a72d870dadb322000646969773d2d7c35fef37fd
SerbCSharp <serbcsharp@mail.ru>
2025-05-07 12:19:54 +0300

    First-r3f-application

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import './style.css'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import Experience from './Experience.jsx'
+import * as THREE from "three"
 
-const root = createRoot(document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-    <div>
-        <App clickersCount={ 4 }></App>
-    </div>
+        <Canvas 
+            //dpr={[ 1, 2 ]}
+            gl={{ antialias: true, toneMapping: THREE.ACESFilmicToneMapping, outputEncoding: THREE.sRGBEncoding }} 
+            camera={{ fov: 45, near: 0.1, far: 200, position: [ 3, 2, 6 ] }}>
+            <Experience />
+        </Canvas>
 )
