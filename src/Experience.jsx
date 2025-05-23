1f1045e760a9ee7a745a958e8f0188d298a6a95d
SerbCSharp <serbcsharp@mail.ru>
2025-05-23 19:44:45 +0300

    Load-models-with-r3f

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,77 +1,31 @@
-import { useFrame } from '@react-three/fiber'
-import { Sky, useHelper, OrbitControls } from '@react-three/drei'
-import { useRef } from 'react'
+import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import * as THREE from 'three'
-import { useControls } from 'leva'
+import { Suspense } from 'react'
+import Model from './Model.jsx'
+import Placeholder from './Placeholder.jsx'
+import Hamburger from './Hamburger.jsx'
+import Fox from './Fox.jsx'
 
 export default function Experience()
 {
-    const directionalLight = useRef()
-    useHelper(directionalLight, THREE.DirectionalLightHelper, 1)
-
-    const { color, opacity, blur } = useControls('contact shadows', {
-        color: '#1d8f75',
-        opacity: { value: 0.4, min: 0, max: 1 },
-        blur: { value: 2.8, min: 0, max: 10 },
-    })
-
-    const { sunPosition } = useControls('sky', {
-        sunPosition: { value: [ 1, 2, 3 ] }
-    })
-
-    const { envMapIntensity, envMapHeight, envMapRadius, envMapScale } = useControls('environment map', {
-        envMapIntensity: { value: 7, min: 0, max: 12 },
-        envMapHeight: { value: 7, min: 0, max: 100 },
-        envMapRadius: { value: 28, min: 10, max: 1000 },
-        envMapScale: { value: 100, min: 10, max: 1000 }
-    })
-
-    const cube = useRef()
-    
-    useFrame((state, delta) =>
-    {
-        cube.current.rotation.y += delta * 0.2
-    })
-
     return <>
-
-        <color args={ [ 'ivory' ] } attach="background" />
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
-        
-        <Sky sunPosition={ sunPosition } />
 
-        <directionalLight
-            ref={ directionalLight }
-            position={ sunPosition }
-            intensity={ 4.5 }
-            castShadow
-            shadow-mapSize={ [ 1024, 1024 ] }
-            shadow-camera-near={ 1 }
-            shadow-camera-far={ 10 }
-            shadow-camera-top={ 5 }
-            shadow-camera-right={ 5 }
-            shadow-camera-bottom={ - 5 }
-            shadow-camera-left={ - 5 }
-        /> 
-        <ambientLight intensity={ 1.5 } />
-
-        <mesh castShadow position-y={ 1 } position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" envMapIntensity={ envMapIntensity } />
-        </mesh>
-
-        <mesh castShadow position-y={ 1 } ref={ cube } position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" envMapIntensity={ envMapIntensity } />
-        </mesh>
+        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } shadow-normalBias={ 0.04 } />
+        <ambientLight intensity={ 0.5 } />
 
         <mesh receiveShadow position-y={ 0 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
-            <meshStandardMaterial color="greenyellow" envMapIntensity={ envMapIntensity } />
+            <meshStandardMaterial color="greenyellow" />
         </mesh>
+
+        {/* <Suspense fallback={ <Placeholder position-y={ 0.5 } scale={ [2, 3, 2] } /> }>
+            <Hamburger scale={ 0.35 } />
+        </Suspense> */}
+
+        <Fox />
     </>
 }
