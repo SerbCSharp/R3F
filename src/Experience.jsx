706f45bbd001109badefb51814c326232adbf0df
SerbCSharp <serbcsharp@mail.ru>
2025-05-25 19:56:14 +0300

    3d-text-with-r3f

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,31 +1,68 @@
-import { OrbitControls } from '@react-three/drei'
+import { useMatcapTexture, Center, Text3D, OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { Suspense } from 'react'
-import Model from './Model.jsx'
-import Placeholder from './Placeholder.jsx'
-import Hamburger from './Hamburger.jsx'
-import Fox from './Fox.jsx'
+import { useRef, useEffect, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
+
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32)
+const material = new THREE.MeshMatcapMaterial()
 
 export default function Experience()
 {
+    const donuts = useRef([])
+    // const donutsGroup = useRef()
+    // const [ torusGeometry, setTorusGeometry] = useState()
+    // const [ material, setMaterial] = useState()
+    const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
+
+    useEffect(() => {
+        matcapTexture.encoding = THREE.sRGBEncoding
+        matcapTexture.needsUpdate = true
+        material.matcap = matcapTexture
+        material.needsUpdate = true
+    }, [])
+
+    useFrame((state, delta) =>
+    {
+        for(const donut of donuts.current)
+        {
+            donut.rotation.y += delta * 0.2
+        }
+    })
+
     return <>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
+        {/* <torusGeometry ref={setTorusGeometry} args={[ 1, 0.6, 16, 32 ]} />
+        <meshMatcapMaterial ref={setMaterial} matcap={ matcapTexture } /> */}
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } shadow-normalBias={ 0.04 } />
-        <ambientLight intensity={ 0.5 } />
+        <Center>
+            <Text3D font="./fonts/helvetiker_regular.typeface.json"
+                size={ 0.75 }
+                height={0.2 }
+                curveSegments={ 12 }
+                bevelEnabled
+                bevelThickness={ 0.02 }
+                bevelSize={ 0.02 }
+                bevelOffset={ 0 }
+                bevelSegments={ 5 }
+                material={material}
+            >
+                Hello R3F
+            </Text3D>
+        </Center>
+        {/* <group ref={donutsGroup}> */}
+            {[...Array(100)].map((value, index) => 
+                <mesh key={index} position={[(Math.random() - 0.5) * 10, (Math.random() - 0.5) * 10, (Math.random() - 0.5) * 10]}
+                    scale={ 0.2 + Math.random() * 0.2}
+                    rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
+                    geometry={torusGeometry}
+                    material={material}
+                    ref={(element) => donuts.current[index] = element} />
+            )}
+            {/* </group> */}
 
-        <mesh receiveShadow position-y={ 0 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
-        {/* <Suspense fallback={ <Placeholder position-y={ 0.5 } scale={ [2, 3, 2] } /> }>
-            <Hamburger scale={ 0.35 } />
-        </Suspense> */}
-
-        <Fox />
     </>
 }
